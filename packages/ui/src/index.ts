988c9c1ff14191f37ad0c226a7c144e7252b877f
penguyen72 <peytonn72@gmail.com>
2026-08-20 18:00:55 -0700

    feat(ui): add Reveal scroll/mount animation primitive

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,5 @@
 export { SiteShell } from "./site-shell";
 export { Button, buttonVariants } from "./components/ui/button";
 export type { ButtonProps } from "./components/ui/button";
+export { Reveal } from "./components/reveal";
+export type { RevealProps } from "./components/reveal";
